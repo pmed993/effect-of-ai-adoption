946f73732c9f1ee9beda962abb6bc7e7d7717305
Pio <pmed993@gmail.com>
2026-04-07 11:20:35 +0100

    feat: get lookup cik and current year rather than cik and fiscal year

---
+++ code/get_ai_adoption/get_cik_year.R
@@ -0,0 +1,67 @@
+setwd("/Users/piomedolla/Desktop/effect-of-genai")
+
+# ---- Packages ----
+packages <- c("data.table", "DBI", "RPostgres", "httr", "tidyverse")
+install.packages(setdiff(packages,rownames(installed.packages())))
+sapply(packages, require, character.only=TRUE)
+
+# ---- Helpers ----
+normalize_cik <- function(x) {
+  # CIKs in master.idx are numeric-ish; this normalizes and removes leading zeros
+  out <- suppressWarnings(as.integer(x))
+  as.character(out)
+}
+
+# ---- Config -----
+OUTDIR <- "cache/"
+if (!dir.exists(OUTDIR)) {dir.create(OUTDIR, recursive = TRUE)}
+setwd(OUTDIR)
+
+# ---- WRDS / Compustat connection ----
+con <- dbConnect(
+  Postgres(),
+  host = "wrds-pgdata.wharton.upenn.edu",
+  port = 9737,
+  user = Sys.getenv("COMPUSTAT_USER"),
+  password = Sys.getenv("COMPUSTAT_PSW"),
+  sslmode = "require",
+  dbname = "wrds"
+)
+on.exit(try(dbDisconnect(con), silent = TRUE), add = TRUE)
+
+# ---- Pull Compustat + CIK list ----
+comp_query <- "
+SELECT 
+  a.gvkey,
+  b.cik,
+  a.datadate,
+  EXTRACT(YEAR FROM a.datadate) AS cyear,
+  a.fyear,
+  b.conm,
+  b.sic
+FROM comp.funda AS a
+LEFT JOIN comp.company AS b
+  ON a.gvkey = b.gvkey
+WHERE a.indfmt = 'INDL'
+  AND a.datafmt = 'STD'
+  AND a.popsrc = 'D'
+  AND a.consol = 'C'
+  AND a.datadate >= '2010-01-01'
+  AND b.sic IS NOT NULL
+  AND b.sic::integer NOT BETWEEN 4900 AND 4999
+  AND b.sic::integer < 6000
+"
+
+# Extract data from Compustat
+comp_data <- dbGetQuery(con, comp_query)
+setDT(comp_data)
+
+# Normalise cik and tidy data
+comp_data[, cik := normalize_cik(cik)]
+comp_data <- comp_data[!is.na(cik) & !is.na(cyear)] # important: drop bad CIKs early
+
+cik_year <- unique(comp_data[, .(cik, year = as.integer(cyear))])
+
+# Cannot run the full extraction as will occupy around 120GB of memory
+saveRDS(cik_year, "cik_year.rds")
+message("cik_year rows: ", nrow(cik_year))
